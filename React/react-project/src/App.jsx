496b18526cf296c9e62f1c65acf2fecf1fa239c3
Anjali Varshney <114928698+Anjalivarshney22@users.noreply.github.com>
2024-11-19 10:30:21 +0530

    React Basics

---
+++ React/react-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from '../Components/Navbar';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Other components go here */}
+
+    </div>
+  );
+}
+
+export default App;
